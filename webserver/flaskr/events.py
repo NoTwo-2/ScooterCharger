df40457b0cf1e00d76e6fb791dce244399005fcf
NowoTwo <cobjaysierko@gmail.com>
2024-02-27 15:32:31 -0600

    updated events, prep for custom events and server to client communication

--- webserver/flaskr/events.py
+++ webserver/flaskr/events.py
@@ -1,7 +1,24 @@
-from flask_socketio import SocketIO
+from flask import Flask, request
+from flask_socketio import SocketIO, emit, disconnect
 
 socketio = SocketIO(cors_allowed_origins="*")
 
+connected_clients = []
+
+# TODO: Class for locker
+
 @socketio.on("connect")
 def handle_connect():
-    print(f"SocketIO Connection Established")
+    connected_clients.append(request.sid)
+    print(f"SocketIO connection established with sid: {request.sid}")
+    
+# TODO: Event for pi to authenticate and send locker info (setup)
+    
+@socketio.on("disconnect")
+def handle_disconnect():
+    connected_clients.remove(request.sid)
+    print(f"SocketIO connection terminated with sid: {request.sid}")
+
+# TODO: Event for pi to send status to server (possibly default message event)
+
+# TODO: Method for finding sid based on locker info
