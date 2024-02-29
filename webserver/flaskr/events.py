f84154471a9d25846840cc6939483c12dfd9067a
NowoTwo <cobjaysierko@gmail.com>
2024-02-29 14:03:21 -0600

    socketio init event started

--- webserver/flaskr/events.py
+++ webserver/flaskr/events.py
@@ -1,22 +1,108 @@
 from flask import Flask, request
-from flask_socketio import SocketIO, emit, disconnect
+
+from flask_socketio import SocketIO, emit, disconnect, send
+
+from flaskr.sqlite_db import get_db
+
+# TODO: Use .env or something similar to handle this
+SECRET = "dev"
 
 socketio = SocketIO(cors_allowed_origins="*")
 
-connected_clients = []
+# class LockerSpace:
+#     pass
 
-# TODO: Class for locker
+class Locker:
+    # locker_list: LockerSpace = []
+    
+    def __init__(
+        self,
+        client_sid: str,
+        id: int = None,
+        num_lockers: int = 2
+    ) -> None:
+        self.client_sid = client_sid
+        self.num_lockers = num_lockers
+        self.id = id
+    
+    def __str__(self) -> str:
+        return f"ID: {self.id} <-> SID: {self.client_sid}"
+
+connected_clients: "list[Locker]" = []
+
+################
+# UTIL FUNCTIONS
+################
+
+def resolve_sid(sid: str) -> Locker:
+    '''
+    Takes an SID and finds the corresponding Locker object
+    '''
+    for client in connected_clients:
+        if client.client_sid == sid:
+            return client
+    return None
+
+################
+# EVENT HANDLERS
+################
 
 @socketio.on("connect")
 def handle_connect():
-    connected_clients.append(request.sid)
+    new_locker = Locker(request.sid)
+    connected_clients.append(new_locker)
     print(f"SocketIO connection established with sid: {request.sid}")
+
+@socketio.on("init")
+def handle_init(json):
+    '''
+    Client sends:
+    {
+        "auth"          : "<INSERT SECRET HERE>",
+        "id"            : "3", 
+        "num_lockers"   : "2"
+    }
+    '''
     
-# TODO: Event for pi to authenticate and send locker info (setup)
+    db = get_db()
+    locker = resolve_sid(request.sid)
+    
+    # Check for auth
+    if not ("auth" in json):
+        print(f"SID: {request.sid} - Sent no auth")
+        disconnect()
+        return
+    if json["auth"] != SECRET:
+        print(f"SID: {request.sid} - Sent incorrect auth")
+        disconnect()
+        return
+    
+    # Check for number of lockers
+    if not ("num_lockers" in json):
+        print(f"SID: {request.sid} - Sent no num_locker")
+        disconnect()
+        return
+    locker.num_lockers = json["num_lockers"]
+    
+    # Check for ID
+    if not ("id" in json):
+        # Create new locker entry in database
+        cursor = db.cursor()
+        cursor.execute(f"INSERT INTO LOCKER (L_ON) VALUES (1)")
+        new_id = cursor.lastrowid
+        db.commit()
+        
+        locker.id = new_id
+    else:
+        locker.id = json["id"]
+    
+    print(f"SID: {request.sid}, LID: {locker.id} - Locker initialized")
+    send(f"{locker.id}")
     
 @socketio.on("disconnect")
 def handle_disconnect():
-    connected_clients.remove(request.sid)
+    locker = resolve_sid(request.sid)
+    connected_clients.remove(resolve_sid(request.sid))
     print(f"SocketIO connection terminated with sid: {request.sid}")
 
 # TODO: Event for pi to send status to server (possibly default message event)
