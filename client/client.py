e0ed272f9bb48a66abf8cea02a234e21f3b18aed
DNSchulte21 <drewnschulte@gmail.com>
2024-03-18 17:45:21 -0500

    Tested client on py

--- client/client.py
+++ client/client.py
@@ -1,6 +1,18 @@
 import socketio
 from datetime import datetime
-import time
+import time, os
+
+def find_temp_sensors():
+    temp_sensors = []
+    for i in os.listdir('/sys/bus/w1/devices'):
+        if i != 'w1_bus_master1':
+            temp_sensors.append(i)
+    return temp_sensors
+
+def read_temp(sensor_num):
+    data = open('/sys/bus/w1/devices/' + sensor_num + '/w1_slave').read()
+    temperature = float(data.split("=")[-1])
+    return ((temperature / 1000) *1.8) + 32
 
 # Create a Socket.IO client
 sio = socketio.Client()
@@ -24,13 +36,13 @@ def on_init(data):
     sio.emit('json', {'status_code': 0, "locker_list": lockers})
 
 @sio.on('lock')
-def lock(index):
-    lockers[index]['state'] = "locked"
+def lock(data):
+    lockers[data['index']]['state'] = "locked"
     sio.emit('json', {'status_code': 0, "locker_list": lockers})
 
 @sio.on('unlock')
-def unlock(index):
-    lockers[index]['state'] = "unlocked"
+def unlock(data):
+    lockers[data['index']]['state'] = "unlocked"
     sio.emit('json', {'status_code': 0, "locker_list": lockers})
     
 @sio.event
@@ -40,16 +52,18 @@ def disconnect():
 def send_time_message():
     while True:
         # Get the current time
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        cur_temp = read_temp(temp_sensors[0])
+        lockers[0]["temperature"] = cur_temp
         
         # Emit the message with the current time
-        sio.emit('message', {'content': current_time})
-        print(f"Sent time_message: {current_time}")
+        sio.emit('json', {'status_code': 0, "locker_list": lockers})
+        print(f"Sent time_message: {lockers}")
         
         # Wait for 10 seconds before sending the next message
         time.sleep(10)
 
 if __name__ == '__main__':
+    temp_sensors = find_temp_sensors()
     sio.connect(server_url)
     try:
         send_time_message()
