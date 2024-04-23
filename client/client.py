6d5c65a4a33864f9f99f9f5152662667e400588d
DSchulte21 <drewnschulte@gmail.com>
2024-04-23 18:19:36 -0500

    added shut off for unsafe values

--- client/client.py
+++ client/client.py
@@ -3,48 +3,7 @@ from datetime import datetime
 import time, os
 import RPi.GPIO as GPIO
 import serial, string
-from config import server_url, locker_num, id, lock_pin, outlet_pin
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(lock_pin,GPIO.OUT)
-GPIO.setup(outlet_pin, GPIO.OUT)
-
-def update_config(x):
-    id = x
-    with open('config.py', 'r') as file:
-        lines = file.readlines()
-
-    new_lines = []
-    for line in lines:
-        if line.strip().startswith('id ='):
-            new_lines.append(f'id = {x}\n')
-        else:
-            new_lines.append(line)
-
-    with open('config.py', 'w') as file:
-        file.writelines(new_lines)
-        
-
-# TODO: order IO devices so that they can be accessed via a locker index
-def find_temp_sensors():
-    temp_sensors = []
-    for i in os.listdir('/sys/bus/w1/devices'):
-        if i != 'w1_bus_master1':
-            temp_sensors.append(i)
-    return temp_sensors
-
-def read_temp(sensor_num):
-    data = open('/sys/bus/w1/devices/' + sensor_num + '/w1_slave').read()
-    temperature = float(data.split("=")[-1])
-    return ((temperature / 1000) *1.8) + 32
-
-# Create a Socket.IO client
-sio = socketio.Client()
-
-# TODO: Initialize this list according to how many IO devices are present (is there an automatic way to figure this out via code?)
-lockers = [{"state": "unlocked", "temperature": 0, "current": 0} for x in range(locker_num)]
-initialized = False
+from config import server_url, locker_num, id, lock_pin, outlet_pin, max_curr, max_temp
 
 @sio.event
 def connect():
@@ -64,45 +23,102 @@ def on_init(data):
     global initialized
     initialized = True
 
-@sio.on('lock')
-def lock(data):
-    locker_index = data['index']
-    print(f"Locking locker {locker_index}")
-    GPIO.output(lock_pin, False)
-    lockers[locker_index]['state'] = "locked"
-    sio.emit('json', {'status_code': 0, "locker_list": lockers})
-
 @sio.on('unlock')
 def unlock(data):
     locker_index = data['index']
     print(f"Unlocking locker {locker_index}")
-    GPIO.output(lock_pin, True)
+    GPIO.output(locker[locker_index]["pins"][""], True)
     lockers[locker_index]['state'] = "unlocked"
     sio.emit('json', {'status_code': 0, "locker_list": lockers})
+    time.wait(5)
+    GPIO.output(locker[locker_index]["pins"]["outlet"], False)
+    lockers[locker_index]['state'] = "locked"
     
 @sio.event
 def disconnect():
     print("Disconnected from the server.")
 
+def update_config(x):
+    id = x
+    with open('config.py', 'r') as file:
+        lines = file.readlines()
+
+    new_lines = []
+    for line in lines:
+        if line.strip().startswith('id ='):
+            new_lines.append(f'id = {x}\n')
+        else:
+            new_lines.append(line)
+
+    with open('config.py', 'w') as file:
+        file.writelines(new_lines)
+        
+def find_temp_sensors():
+    temp_sensors = []
+    for i in os.listdir('/sys/bus/w1/devices'):
+        if i != 'w1_bus_master1':
+            temp_sensors.append(i)
+    return temp_sensors
+
+def read_temp(sensor_num):
+    data = open('/sys/bus/w1/devices/' + sensor_num + '/w1_slave').read()
+    temperature = float(data.split("=")[-1])
+    return ((temperature / 1000) *1.8) + 32
+
+
 def update_message():
     while True:
         # Get the current temperature
-        lockers[0]["temperature"] = read_temp(temp_sensors[0])
-    
-        lockers[0]["current"] = str(serial_input.readline())[2:-5]
+        lockers[0]["temperature"] = read_temp(locker[0]["pins"]["temp"])
+        lockers[0]["current"] = str(lockers[0]["pins"]["curr"].readline())[2:-5]
+        
+        # If any values out of bounds shutdown
+        for locker in lockers:
+            if locker["temperature"] > max_temp or locker["curr"] > max_curr:
+                # shut down here
+                GPIO.output(locker[locker_index]["pins"]["outlet"], False)
+                sio.emit('json', {'status_code': 1, "message": f'EMERGENCY {locker} : {locker["temperature"] > {max_temp} , {locker["current"]} {max_curr}', "locker_list": lockers})
+
+        
         
         # Emit the message with the current time
         sio.emit('json', {'status_code': 0, "locker_list": lockers})
         print(f"Sent message: {lockers}")
         
         # Wait for 10 seconds before sending the next message
-        time.sleep()
+        time.sleep(save_rate)
 
 if __name__ == '__main__':
+    # Create a Socket.IO client
+    sio = socketio.Client()
+    initialized = False
+    save_rate = 10
+    
     # Setup for sensors
     temp_sensors = find_temp_sensors()
-    serial_input = serial.Serial('/dev/ttyACM0',9600,8,'N',1,timeout=1)
-    save_rate = 10
+    
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(lock_pin,GPIO.OUT)
+    GPIO.setup(outlet_pin, GPIO.OUT)
+    
+    # Set pins
+    locker[0]["pins"]["temp"] = temp_sensors[0]
+    locker[0]["pins"]["curr"] = serial.Serial('/dev/ttyACM0',9600,8,'N',1,timeout=1)
+    locker[0]["pins"]["lock"] = 17
+    locker[0]["pins"]["outlet"] = 27
+
+    # TODO: Initialize this list according to how many IO devices are present (is there an automatic way to figure this out via code?)
+    lockers = [{
+        "state": "unlocked",
+        "temperature": 0,
+        "current": 0,
+        "sensors":{
+            "temp":0,
+            "curr":0,
+            "lock":0,
+            "outlet":0}
+    } for x in range(locker_num)]
     
     # Connect to server
     print(f"Attempting connection to {server_url}...")
--- client/config.py
+++ client/config.py
@@ -1,3 +1,5 @@
 server_url = "http://localhost:5000" # Change this to your server's IP address and port
 locker_num = 2
+max_temp = 100
+max_curr = 5
 id = None
