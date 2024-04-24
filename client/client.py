18b77bf99b8d6c9962bd4565909f0c36ce1b62ab
NowoTwo <cobjaysierko@gmail.com>
2024-04-24 05:11:05 -0500

    client.py cleanup, needs attention

--- client/client.py
+++ client/client.py
@@ -3,7 +3,11 @@ from datetime import datetime
 import time, os
 import RPi.GPIO as GPIO
 import serial, string
-from config import server_url, locker_num, id, lock_pin, outlet_pin, max_curr, max_temp
+from config import server_url, locker_num, id, lock_pin, outlet_pin, max_curr, max_temp # NOTE: Remnants? Needs attention..
+
+###############
+# EVENTS
+###############
 
 @sio.event
 def connect():
@@ -19,6 +23,7 @@ def on_init(data):
     print(f"Server says: {data}")
     update_config(data['id'])
     # Write id to config.py no matter what.
+    global save_rate
     save_rate = int(data['status_rate'])
     global initialized
     initialized = True
@@ -27,19 +32,20 @@ def on_init(data):
 def unlock(data):
     locker_index = data['index']
     print(f"Unlocking locker {locker_index}")
-    GPIO.output(locker[locker_index]["pins"][""], True)
-    lockers[locker_index]['state'] = "unlocked"
+    GPIO.output(lockers[locker_index]["pins"]["outlet"], True)
     sio.emit('json', {'status_code': 0, "locker_list": lockers})
     time.wait(5)
-    GPIO.output(locker[locker_index]["pins"]["outlet"], False)
-    lockers[locker_index]['state'] = "locked"
+    GPIO.output(lockers[locker_index]["pins"]["outlet"], False)
     
 @sio.event
 def disconnect():
     print("Disconnected from the server.")
 
+###############
+# UTIL
+###############
+
 def update_config(x):
-    id = x
     with open('config.py', 'r') as file:
         lines = file.readlines()
 
@@ -65,28 +71,53 @@ def read_temp(sensor_num):
     temperature = float(data.split("=")[-1])
     return ((temperature / 1000) *1.8) + 32
 
-
 def update_message():
     while True:
-        # Get the current temperature
-        lockers[0]["temperature"] = read_temp(locker[0]["pins"]["temp"])
-        lockers[0]["current"] = str(lockers[0]["pins"]["curr"].readline())[2:-5]
-        
+        # NOTE: shouldnt this be getting checked more frequently?
         # If any values out of bounds shutdown
+        unsafe_temps = ""
+        unsafe_currs = ""
         for locker in lockers:
-            if locker["temperature"] > max_temp or locker["curr"] > max_curr:
-                # shut down here
-                GPIO.output(locker[locker_index]["pins"]["outlet"], False)
-                sio.emit('json', {'status_code': 1, "message": f'EMERGENCY {locker} : {locker["temperature"] > {max_temp} , {locker["current"]} {max_curr}', "locker_list": lockers})
-
+            # Get the most recent sensor values
+            locker["temperature"] = read_temp(locker["pins"]["temp"])
+            locker["current"] = str(locker["pins"]["curr"].readline())[2:-5]
+            
+            unsafe_temp = locker["temperature"] > max_temp
+            unsafe_current = locker["current"] > max_curr
+            if (not unsafe_temp) and (not unsafe_current):
+                GPIO.output(locker["pins"]["outlet"], True)
+                locker["state"] = "good"
+                continue
+            
+            # shut down here
+            GPIO.output(locker["pins"]["outlet"], False)
+            locker["state"] = "disabled"
+            
+            # append datas
+            if unsafe_temp:
+                unsafe_temps += f"ID: {lockers.index(locker)} - {locker["temperature"]}\n"
+            if unsafe_current:
+                unsafe_currs += f"ID: {lockers.index(locker)} - {locker["current"]}\n"
         
+        # Emit the json event
+        if unsafe_currs == "" and unsafe_temps == "":
+            sio.emit('json', {'status_code': 0, "locker_list": lockers})
+        else:
+            final_message = (
+                f"One or more lockers were disabled due to unsafe conditions\n\n"
+                f"Lockers that exceeded a maximum safe temperature of {max_temp}:\n{unsafe_temps}\n"
+                f"Lockers that exceeded a maximum safe current of {max_curr}:\n{unsafe_currs}"
+            )
+            sio.emit('json', {'status_code': 1, "error_msg": final_message, "locker_list": lockers})
         
-        # Emit the message with the current time
-        sio.emit('json', {'status_code': 0, "locker_list": lockers})
-        print(f"Sent message: {lockers}")
+        print(f"Sent JSON")
         
-        # Wait for 10 seconds before sending the next message
+        # Loop periodically based on save_rate
         time.sleep(save_rate)
+
+###############
+# INIT
+###############
 
 if __name__ == '__main__':
     # Create a Socket.IO client
@@ -99,18 +130,12 @@ if __name__ == '__main__':
     
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(lock_pin,GPIO.OUT)
+    GPIO.setup(lock_pin,GPIO.OUT) # NOTE: these are depricated, i dont know where they come from now?
     GPIO.setup(outlet_pin, GPIO.OUT)
-    
-    # Set pins
-    locker[0]["pins"]["temp"] = temp_sensors[0]
-    locker[0]["pins"]["curr"] = serial.Serial('/dev/ttyACM0',9600,8,'N',1,timeout=1)
-    locker[0]["pins"]["lock"] = 17
-    locker[0]["pins"]["outlet"] = 27
 
     # TODO: Initialize this list according to how many IO devices are present (is there an automatic way to figure this out via code?)
     lockers = [{
-        "state": "unlocked",
+        "state": "good",
         "temperature": 0,
         "current": 0,
         "sensors":{
@@ -120,11 +145,17 @@ if __name__ == '__main__':
             "outlet":0}
     } for x in range(locker_num)]
     
+    # Set pins for each locker
+    lockers[0]["pins"]["temp"] = temp_sensors[0]
+    lockers[0]["pins"]["curr"] = serial.Serial('/dev/ttyACM0',9600,8,'N',1,timeout=1)
+    lockers[0]["pins"]["lock"] = 17
+    lockers[0]["pins"]["outlet"] = 27
+    
     # Connect to server
     print(f"Attempting connection to {server_url}...")
-    sio.connect(server_url)
+    sio.connect(server_url, retry=True)
     while not initialized:
-       continue
+        continue
     
     # Main task executions
     print("Finished initialization")
